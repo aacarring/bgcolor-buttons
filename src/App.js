f61688c1d0fd6a68a1f2e2aa93e9dbfdc1efee37
Alexa Carrington <acarring@wellesley.edu>
2020-12-22 12:20:28 -0500

    Add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          backgroundColor: "white"
+        };
+        this.handleClickRed = this.handleClickRed.bind(this);
+        this.handleClickOrange = this.handleClickOrange.bind(this);
+        this.handleClickYellow = this.handleClickYellow.bind(this);
+    }
+
+handleClickRed(e) {
+  this.setState({
+    backgroundColor: "red"
+  });
+}
+handleClickOrange(e) {
+  this.setState({
+    backgroundColor: "orange"
+  });
+}
+handleClickYellow(e) {
+  this.setState({
+    backgroundColor: "yellow"
+  });
 }
 
-export default App;
+render() {
+    return (
+        <div style={{backgroundColor: this.state.backgroundColor}}>
+            <button onClick={this.handleClickRed}>Red</button>
+            <button onClick={this.handleClickOrange}>Orange</button>
+            <button onClick={this.handleClickYellow}>Yellow</button>
+        </div>
+    )
+}
+
+}
+
+export default App
